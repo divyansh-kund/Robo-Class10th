7bc004b07043905d942cacfd4d9e4107c7fa0f94
divyansh-kund <divyanshkundu8@gmail.com>
2020-07-07 12:08:32 +0530

    Added Dev Mehrotra name

---
+++ precache-manifest.f3f14b2efa8749e356e833bba5814b9b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f3a863589407b6cd6c8225bd31ccc34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ab2181a9b75d57837ce",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "0e534c1c42aa3181a916",
+    "url": "/static/css/main.aba07943.chunk.css"
+  },
+  {
+    "revision": "6ab2181a9b75d57837ce",
+    "url": "/static/js/2.6bb3f014.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bb3f014.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e534c1c42aa3181a916",
+    "url": "/static/js/main.6aac7046.chunk.js"
+  },
+  {
+    "revision": "aca0f5dabab3afe36e0f",
+    "url": "/static/js/runtime-main.e96107c2.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
